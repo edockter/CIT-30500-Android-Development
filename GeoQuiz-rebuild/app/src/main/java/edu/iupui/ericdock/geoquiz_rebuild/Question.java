785ba503496af4021c29be2a680ba2eaf13e96db
Eric Dockter <eric.dockter@gmail.com>
2017-01-17 22:58:47 -0500

    Completed chapter 1 + 2 -- Basics + MVC. * Wired up 4 buttons. * Added image resources, added and used text strings. * Utilized controller for app logic. * Created new Question class to store Question string resource IDs and true/false correct info. * Completed Challenges 1 + 2 -- did not like the ImageButtons in Challenge 3. ** Added Prev button in Challenges.  Added a check to roll to end of array if we're at the last question. ** Added click listener to TextView that goes to the next question.

---
+++ GeoQuiz-rebuild/app/src/main/java/edu/iupui/ericdock/geoquiz_rebuild/Question.java
@@ -0,0 +1,32 @@
+package edu.iupui.ericdock.geoquiz_rebuild;
+
+/**
+ * Created by Eric on 1/17/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
