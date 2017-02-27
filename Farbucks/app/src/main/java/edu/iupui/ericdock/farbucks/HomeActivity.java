8d1de14b4fcab0f400050b77c9aa099fdeca659d
Eric Dockter <ericdock@iupui.edu>
2017-02-27 17:20:19 -0500

    In-class changes 2/27:   Add app drawer and navigation menu

--- Farbucks/app/src/main/java/edu/iupui/ericdock/farbucks/HomeActivity.java
+++ Farbucks/app/src/main/java/edu/iupui/ericdock/farbucks/HomeActivity.java
@@ -4,7 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends SingleFragmentActivity {
 
     protected Fragment createFragment() {
         return new HomeFragment();
