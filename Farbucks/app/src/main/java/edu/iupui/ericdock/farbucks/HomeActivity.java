9a738ad2e5aab2f4408f3170dd7294a0bdc9dca6
Eric Dockter <eric.dockter@gmail.com>
2017-02-27 01:01:32 -0500

    Completed Assignment: Farbucks, v.1. Skeleton of app.

---
+++ Farbucks/app/src/main/java/edu/iupui/ericdock/farbucks/HomeActivity.java
@@ -0,0 +1,12 @@
+package edu.iupui.ericdock.farbucks;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    protected Fragment createFragment() {
+        return new HomeFragment();
+    }
+}
---
+++ Farbucks/app/src/main/java/edu/iupui/ericdock/farbucks/HomeFragment.java
@@ -0,0 +1,32 @@
+package edu.iupui.ericdock.farbucks;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Eric on 2/27/2017.
+ */
+
+public class HomeFragment extends Fragment {
+    private TextView mHomeTitleTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mHomeTitleTextView = (TextView) view.findViewById(R.id.home_title_textview);
+
+        return view;
+    }
+}
