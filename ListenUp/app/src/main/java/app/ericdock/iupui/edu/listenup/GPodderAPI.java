c3cc3047200b13c68f31551ab5be0117026457ed
Eric Dockter <ericdock@iupui.edu>
2017-05-02 00:53:38 -0400

    ListenUp Progress

--- ListenUp/app/src/main/java/app/ericdock/iupui/edu/listenup/GPodderAPI.java
+++ ListenUp/app/src/main/java/app/ericdock/iupui/edu/listenup/GPodderAPI.java
@@ -1,9 +1,15 @@
 package app.ericdock.iupui.edu.listenup;
 
+import java.util.List;
+
 import app.ericdock.iupui.edu.listenup.model.Podcast;
 import app.ericdock.iupui.edu.listenup.model.Tag;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 /**
@@ -24,4 +30,26 @@ public interface GPodderAPI {
 
     @GET("api/2/devices/{username}.json")
     Call<String> getDevices(@Path("username") String username);
+
+    // Login
+    @POST("/api/2/auth/{username}/login.json")
+        //void login(@Path("user") String user, @Header("Authorization") String authorization, Callback<String> callback);
+    Call<ResponseBody> login(@Path("username") String user);
+
+    // Get Subscriptions of Device
+    @GET("/subscriptions/{username}/{deviceid}.json")
+    Call<String[]> getDeviceSubscriptions(@Path("username") String user, @Path("deviceid") String device);
+
+    // Get Subscriptions
+    @GET("/subscriptions/{username}.json")
+    Call<List<Subscription>> getSubscriptions(@Path("username") String user);
+
+    // Upload Subscriptions of Device
+    @PUT("/subscriptions/{username}/{deviceid}.json")
+    Call<ResponseBody> uploadDeviceSubscriptions(@Body List<String> subscriptions, @Path("username") String user, @Path("deviceid") String device);
+
+    // Upload Subscription Changes
+    @POST("/api/2/subscriptions/{username}/{deviceid}.json")
+    Call<UpdatedUrls> uploadDeviceSubscriptionsChanges(@Body SubscriptionChanges subscriptionChanges, @Path("username") String user, @Path("deviceid") String device);
+
 }
--- ListenUp/app/src/main/java/app/ericdock/iupui/edu/listenup/LoginFragment.java
+++ ListenUp/app/src/main/java/app/ericdock/iupui/edu/listenup/LoginFragment.java
@@ -1,26 +1,39 @@
 package app.ericdock.iupui.edu.listenup;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import org.w3c.dom.Text;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
 
 /**
  * Created by ericd on 4/26/2017.
  */
 
 public class LoginFragment extends Fragment {
-    private TextView mUsernameTextview;
-    private TextView mPasswordTextview;
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
     private Button mLoginButton;
     private String mUsername;
     private String mPassword;
+    private boolean mLoginSuccessful = false;   // Start with false, true if login works
+
+    private final String USERNAME_KEY = "gpodder_username";
+    private final String PASSWORD_KEY = "gpodder_password";
+    private final String BOOLEAN_KEY = "gpodder_login_status";
+    private final String DEVICE_KEY = "gpodder_device_id";
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -31,8 +44,36 @@ public class LoginFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_taglist, container, false);
 
         // wire up
-        mUsernameTextview = (TextView) view.findViewById(R.id.login_username_edittext);
-        mPasswordTextview = (TextView) view.findViewById(R.id.login_password_edittext);
+        mUsernameEditText = (EditText) view.findViewById(R.id.login_username_edittext);
+        mPasswordEditText = (EditText) view.findViewById(R.id.login_password_edittext);
+        mLoginButton = (Button) view.findViewById(R.id.login_button);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                mUsername = mUsernameEditText.getText().toString();
+                mPassword = mPasswordEditText.getText().toString();
+
+                GPodderAPI client = ServiceGenerator.createService(GPodderAPI.class, mUsername, mPassword);
+                Call<ResponseBody> call = client.login(mUsername);
+
+
+                if (mLoginSuccessful) {
+                    // Write credentials to sharedpreferences
+                    SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString(USERNAME_KEY, mUsername);
+                    editor.putString(PASSWORD_KEY, mPassword);
+                    editor.putBoolean(BOOLEAN_KEY, true);
+                    editor.putString(DEVICE_KEY, "device-id");
+                    editor.commit();
+                }
+                else {
+                    Toast.makeText(getContext(), "Login unsuccessful.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         return view;
     }
